d4e2057d9fdcf35435812c76c25134da55f84bb4
Ihsan Faisal <ihsan@sisdik.com>
2025-05-26 11:25:24 +0700

    Menambah konfigurasi firestore dan fungsi daftar todo

---
+++ main.js
@@ -0,0 +1,50 @@
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js';
+
+import {
+    getFirestore,
+    collection,
+    doc,
+    getDocs,
+    getDoc,
+    addDoc,
+    deleteDoc,
+    updateDoc,
+    query,
+    orderBy
+} from 'https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDFYmmVvk-jLZIeAdYKiTwVw2jqd4VINFA",
+    authDomain: "insan-cemerlang.firebaseapp.com",
+    projectId: "insan-cemerlang",
+    storageBucket: "insan-cemerlang.appspot.com",
+    messagingSenderId: "579109661574",
+    appId: "1:579109661574:web:4a7cd4060f70eded945a07"
+};
+
+//inisialisasi firebase
+const aplikasi = initializeApp(firebaseConfig);
+const basisdata = getFirestore(aplikasi);
+
+// tambahkan fungsi untuk menampilkan daftar todo list
+export async function ambilDaftarTodo() {
+    try {
+        const refDokumen = collection(basisdata, "todo");
+        const kueri = query(refDokumen, orderBy("teks"));
+        const cuplikanKueri = await getDocs(kueri);
+
+        let hasilKueri = [];
+        cuplikanKueri.forEach((dokumen) => {
+            hasilKueri.push({
+                id: dokumen.id,
+                teks: dokumen.data().teks,
+                status: dokumen.data().status
+            });
+        });
+
+        return hasilKueri;
+    } catch (error) {
+        console.error("Gagal mengambil daftar to-do:", error);
+        return [];
+    }
+}
